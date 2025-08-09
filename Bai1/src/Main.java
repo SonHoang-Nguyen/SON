d7eb9313840d5c3a372ca79ea0c2393dcec5d237
hoang <hoangson080906@gmail.com>
2025-08-09 14:59:57 +0700

    bai2 tim so lon nhat

--- Bai1/src/Main.java
+++ Bai1/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = 100;
+        int a = 217;
         System.out.println(a);
     }
 }
