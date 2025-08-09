a85c6073769c536857398038c2d5918cb71a8261
hoang <hoangson080906@gmail.com>
2025-08-09 15:20:24 +0700

    b1 tim so trong mang

--- Bai1/src/Main.java
+++ Bai1/src/Main.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = 100;
-        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int k = sc.nextInt();
+        int [] arr = new int[k];
+        for (int i = 0; i < k; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < k; i++) {
+            if(arr[i] == a) {
+                System.out.println(i);
+            }
+        }
+
     }
 }
