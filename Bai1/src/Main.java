c1f0d01c3aa1a28256a57f44e8d88853efd5dfba
hoang <hoangson080906@gmail.com>
2025-09-06 15:46:54 +0700

    tim so le o vi tri chan

--- Bai1/src/Main.java
+++ Bai1/src/Main.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = 100;
-        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0 && a[i] % 2 != 0) {
+                System.out.print(a[i] + " ");
+                x = x + 1;
+            }
+        }
+        if(x == 0) {
+            System.out.println("NULL");
+        }
     }
 }
+
+
+
+
+
+
+
